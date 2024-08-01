930cc4e7c6392864951eaa772b4d9b2478eda2ef
nusratjahanoishi <thisisoishi@gmail.com>
2024-08-02 00:07:26 +0600

    Refactor codes

--- frontend/src/Routes.js
+++
@@ -1,9 +0,0 @@
-import Login from"../src/pages/Login.jsx";
-import SignupPage from "./pages/SignupPage.jsx"
-import HomePage from './pages/Home.jsx';
-
-export{
-    Login,
-    SignupPage,
-    HomePage
-}
